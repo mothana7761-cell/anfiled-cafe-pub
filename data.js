df103385d9a7d3573244d7e85cc4aa49147bbe18
mothana7761-cell <mothana7761@gmail.com>
2026-06-29 23:55:40 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,175 @@
+const categories = [
 
+{
+    id:"breakfast",
+    name:{
+        en:"Breakfast",
+        ar:"الفطور",
+        tr:"Kahvaltı"
+    },
+    image:"images/categories/breakfast.jpg"
+},
+
+{
+    id:"hot-starters",
+    name:{
+        en:"Hot Starters",
+        ar:"المقبلات الساخنة",
+        tr:"Sıcak Başlangıçlar"
+    },
+    image:"images/categories/hot.jpg"
+},
+
+{
+    id:"cold-meze",
+    name:{
+        en:"Cold Meze",
+        ar:"المقبلات الباردة",
+        tr:"Soğuk Mezeler"
+    },
+    image:"images/categories/meze.jpg"
+},
+
+{
+    id:"salads",
+    name:{
+        en:"Salads",
+        ar:"السلطات",
+        tr:"Salatalar"
+    },
+    image:"images/categories/salad.jpg"
+},
+
+{
+    id:"sandwiches",
+    name:{
+        en:"Sandwiches",
+        ar:"الساندويشات",
+        tr:"Sandviçler"
+    },
+    image:"images/categories/sandwich.jpg"
+},
+
+{
+    id:"wraps",
+    name:{
+        en:"Wraps",
+        ar:"الراب",
+        tr:"Wrap"
+    },
+    image:"images/categories/wrap.jpg"
+},
+
+{
+    id:"burgers",
+    name:{
+        en:"Burgers",
+        ar:"البرغر",
+        tr:"Burgerler"
+    },
+    image:"images/categories/burger.jpg"
+},
+
+{
+    id:"pizza",
+    name:{
+        en:"Pizza",
+        ar:"البيتزا",
+        tr:"Pizza"
+    },
+    image:"images/categories/pizza.jpg"
+},
+
+{
+    id:"pasta",
+    name:{
+        en:"Pasta",
+        ar:"الباستا",
+        tr:"Makarna"
+    },
+    image:"images/categories/pasta.jpg"
+},
+
+{
+    id:"grill",
+    name:{
+        en:"Grill",
+        ar:"المشاوي",
+        tr:"Izgara"
+    },
+    image:"images/categories/grill.jpg"
+},
+
+{
+    id:"desserts",
+    name:{
+        en:"Desserts",
+        ar:"الحلويات",
+        tr:"Tatlılar"
+    },
+    image:"images/categories/dessert.jpg"
+},
+
+{
+    id:"coffee",
+    name:{
+        en:"Coffee",
+        ar:"القهوة",
+        tr:"Kahve"
+    },
+    image:"images/categories/coffee.jpg"
+},
+
+{
+    id:"hot-drinks",
+    name:{
+        en:"Hot Drinks",
+        ar:"المشروبات الساخنة",
+        tr:"Sıcak İçecekler"
+    },
+    image:"images/categories/hotdrinks.jpg"
+},
+
+{
+    id:"cold-drinks",
+    name:{
+        en:"Cold Drinks",
+        ar:"المشروبات الباردة",
+        tr:"Soğuk İçecekler"
+    },
+    image:"images/categories/drinks.jpg"
+},
+
+{
+    id:"cocktails",
+    name:{
+        en:"Cocktails",
+        ar:"الكوكتيلات",
+        tr:"Kokteyller"
+    },
+    image:"images/categories/cocktail.jpg"
+},
+
+{
+    id:"alcohol",
+    name:{
+        en:"Alcohol",
+        ar:"المشروبات الكحولية",
+        tr:"Alkollü İçecekler"
+    },
+    image:"images/categories/alcohol.jpg"
+},
+
+{
+    id:"shisha",
+    name:{
+        en:"Shisha",
+        ar:"الأرجيلة",
+        tr:"Nargile"
+    },
+    image:"images/categories/shisha.jpg"
+}
+
+];
+
+const menuItems = [];
